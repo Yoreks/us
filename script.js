038fd96389385dbde07444d0fd0f3564aa411b73
Josue R <64994168+Yoreks@users.noreply.github.com>
2024-03-30 00:00:13 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$("#btn").click(function(){
+    Swal.fire({
+        title: "Felices 6 meses amorcito!",
+        text: "",
+        imageUrl: "pati.gif",
+        imageWidth: 200,
+        imageHeight: 200,
+        imageAlt: "Custom image"
+      });
+});
