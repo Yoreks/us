652d36ad403b910a401887e26ee94a9832fa73ab
Josue R <64994168+Yoreks@users.noreply.github.com>
2024-03-30 20:26:21 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $("#btn").click(function(){
     Swal.fire({
         title: "Felices 6 meses amorcito!",
         text: "",
-        imageUrl: "pati.gif",
+        imageUrl: "patiimg.gif",
         imageWidth: 200,
         imageHeight: 200,
         imageAlt: "Custom image"
